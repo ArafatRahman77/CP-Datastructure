8ea81ec3e1be8f6d2e2aca38ec02a8f1ffa82910
ArafatRahman77 <princearafath77@gmail.com>
2022-07-24 09:49:58 +0600

    first

---
+++ Ratinginprac.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i, flag;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        flag = 0;
+        for (i = 0; i < n - 1; i++)
+        {
+            if (a[i + 1] < a[i])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
